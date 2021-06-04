ace800bbf8f821c6aee7d7d1004e81d63196b792
hemmerling <hemmerling@gmx.net>
2021-06-04 08:10:09 +0200

    Update

--- Data/Scripts/try.lua
+++ Data/Scripts/try.lua
@@ -36,41 +36,18 @@ function testFalseResult1()
 end
 
 function testFalseResult2()
-    lu.assertEquals(add(1,1),1)
+    lu.assertEquals(add(1,2),1)
 end
-
--- print(_ENV["testFalseResult1"])
--- print(_ENV["testFalseResult2"])
--- print(_ENV["testAddZero"])
-
--- print(_ENV == _G)
-
--- print("_G, ursprünglich:")
--- for n in pairs(_G) do print(n) 
--- end
--- print("_ENV, ursprünglich:")
--- for n in pairs(_ENV) do print(n) 
--- end
--- print("xxx")
-
-_G["testAddPositive"] = testAddPositive
-_G["testAddZero"] = testAddZero
-_G["testFalseResult1"] = testFalseResult1
-_G["testFalseResult2"] = testFalseResult2
-
--- _G = _ENV
-
--- print("_G, als Kopie von _ENV:")
--- for n in pairs(_G) do print(n) 
--- end
-
-
--- _ENV["testAddPositive"] = testAddPositive
--- _ENV["testAddZero"] = testAddZero
--- _ENV["testFalseResult"] = testFalseResult
 
 -- ** Start of modification for Lua@CORE **
 if (CoreObject) then
+    -- Copy _ENV table to the _G client table, as _G = _ENV does not work!
+    -- With Lua@CORE, _ENV contains a list of all global symbols ( functions, tables.. )
+    -- With Lua@Core, there is a _G client and a _G server table for each player, both are "empty". 
+    -- _G = _ENV  -- Does not work!
+    for name, address in pairs(_ENV) do 
+        _G[name] = address
+    end   
     lu.LuaUnit.run()
 else
     -- The last line executes your script with LuaUnit and exit with the proper error code:
